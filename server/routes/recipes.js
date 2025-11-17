19cb596f50ec54906e70325af7c62091b70ccc94
saadnavotu <saad.nav@ontariotechu.net>
2025-11-16 19:31:26 -0500

    Finalized Assignmet 3 structure, ready for CRUD implementation

---
+++ server/routes/recipes.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// connect to package model
+let Recipe = require('../model/recipe');
+
+
+module.exports = router;
